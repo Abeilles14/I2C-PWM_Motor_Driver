d112422047b27dbfdfa4aeed363bd22d02df64f7
Abeilles14 <40010108+Abeilles14@users.noreply.github.com>
2022-04-11 10:05:30 -0700

    single brushed driver motro controls work

---
+++ TB9051FTG.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+from constants import *
+from utils import getCounterValues
+import odroid_wiringpi as wpi
+
+class TB9051FTG:
+    # inputs = [encA, encB]
+    # outputs = [ENB, PWM1, PWM2]
+    def __init__(self, channel, freq, pin_in, pin_out, debug=False):
+        self.freq = freq
+        self.current_state = 0 #
+        self.channel = channel
+        self.debug = debug
+
+        # odroid - motor/driver input and output pins
+        self.pin_in = pin_in    # encA encB
+        self.pin_out = pin_out  # enb pwm1 pwm2
+
+    def reset(self, pwm):
+        # reset PCA9685
+        if self.debug:
+            print("Reseting PCA9685")
+        on_count, off_count = getCounterValues(delay=0, dc=0)
+        on_hex, off_hex = int(hex(on_count), base=16), int(hex(off_count), base=16)
+        pwm.setPWMCounters(self.channel, I2C_BUS, on_hex, off_hex)  # EN = 0
+
+        # reset TB9051FTG
+        if self.debug:
+            print("Reseting TB9051FTG")
+
+        # setup wpi
+        wpi.wiringPiSetup()
+        # set pin mode
+        for pin in self.pin_in:
+            wpi.pinMode(pin, IN)
+        for pin in self.pin_out:
+            wpi.pinMode(pin, OUT)
+        # init pins
+        wpi.digitalWrite(self.pin_out[0], 0)    # ENB = 0
+        wpi.digitalWrite(self.pin_out[1], 0)    # PWM1 = 0
+        wpi.digitalWrite(self.pin_out[2], 0)    # PWM2 = 0
+
+    def forward(self, pwm, dutycycle):
+        wpi.digitalWrite(self.pin_out[0], 0)    # ENB = 0
+        wpi.digitalWrite(self.pin_out[1], 1)    # PWM1 = 1
+        wpi.digitalWrite(self.pin_out[2], 0)    # PWM2 = 0
+
+        self.setPWM(pwm, dutycycle)
+    
+    def backward(self, pwm, dutycycle):
+        wpi.digitalWrite(self.pin_out[0], 0)    # ENB = 0
+        wpi.digitalWrite(self.pin_out[1], 0)    # PWM1 = 0
+        wpi.digitalWrite(self.pin_out[2], 1)    # PWM2 = 1
+
+        self.setPWM(pwm, dutycycle)
+    
+    def stop(self, pwm):
+        wpi.digitalWrite(self.pin_out[0], 0)    # ENB = 0
+        wpi.digitalWrite(self.pin_out[1], 0)    # PWM1 = 0
+        wpi.digitalWrite(self.pin_out[2], 0)    # PWM2 = 0
+
+        self.setPWM(pwm, 0)
+
+    def setPWM(self, pwm, dutycycle, delay=0):
+        print("Delay: {}, Duty Cycle: {}".format(delay, dutycycle))
+        on_count, off_count = getCounterValues(delay, dutycycle)
+        on_hex, off_hex = int(hex(on_count), base=16), int(hex(off_count), base=16)
+
+        print("ON: {}, {}, OFF: {}, {}".format(on_count, hex(on_count), off_count, hex(off_count)))
+        pwm.setPWMCounters(self.channel, I2C_BUS, on_hex, off_hex)
+
+
--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 from pwm import PWM
 from constants import *
-from motor_model import Motor
+from TB9051FTG import TB9051FTG
 
 def main():
-    FREQUENCY = 500 # Hz
+    FREQUENCY = 5 # Hz
     pwm = PWM(address=I2C_CHIP, busnum=I2C_BUS,debug=True)
     pwm.setPWMFreq(FREQUENCY)
 
     # actuonixL16 = Motor(channel=CHANNEL0, freq=300)
-    # actuonixPQ12 = Motor(channel=CHANNEL1, freq=300)
-
     # actuonixL16.reset(pwm)
-    # actuonixPQ12.reset(pwm)
-
     # actuonixL16.setPWM(pwm, dutycycle=60)
-    # actuonixPQ12.setPWM(pwm, dutycycle=30)
     
     #### WINCH MOTOR ####
     # GOING FORWARD: PWM1 H PWM2 L EN H ENB L
     # setup up pwm1 and pwm2
-    pololu1 = Motor(channel=CHANNEL2, freq=FREQUENCY)
+    pololu1 = TB9051FTG(channel=CHANNEL2, freq=FREQUENCY, pin_in=POLOLU1_ENC, pin_out=TB9051FTG_PINS)
     pololu1.reset(pwm)
-    pololu1.setPWM(pwm, dutycycle=30)
+    # pololu1.forward(pwm, dutycycle=30)
+
+    while True:
+        direction = input("Enter f for fwd, b for bkwd, s for stop: ")
+        if direction == "f":
+            print("Going forward")
+            pololu1.forward(pwm, dutycycle=30)
+        elif direction == "b":
+            print("Going Backward")
+            pololu1.backward(pwm, dutycycle=30)
+        elif direction == "s":
+            print("Stopping")
+            pololu1.stop(pwm)
+    # pololu1.setPWM(pwm, dutycycle=30)
+
 
 if __name__ == "__main__":
     main()
