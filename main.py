2e82cf9e0e0aae6623888c7f4b8ab942b3b400ae
Abeilles14 <40010108+Abeilles14@users.noreply.github.com>
2022-04-15 02:13:57 -0700

    joystick experiment and analog pins

--- main.py
+++ main.py
@@ -32,28 +32,28 @@ def main():
     # ACTUATORS
     actuonix_1 = PCA9685(channel=CHANNEL8, freq=300)
     actuonix_1.reset(pwm)
-    actuonix_1.setPWM(pwm, dutycycle=60)
+    actuonix_1.setPWM(pwm, dutycycle=30)
 
-    actuonix_2 = PCA9685(channel=CHANNEL9, freq=300)
-    actuonix_2.reset(pwm)
-    actuonix_2.setPWM(pwm, dutycycle=60)
+    # actuonix_2 = PCA9685(channel=CHANNEL9, freq=300)
+    # actuonix_2.reset(pwm)
+    # actuonix_2.setPWM(pwm, dutycycle=60)
 
-    actuonix_3 = PCA9685(channel=CHANNEL10, freq=300)
-    actuonix_3.reset(pwm)
-    actuonix_3.setPWM(pwm, dutycycle=60)
+    # actuonix_3 = PCA9685(channel=CHANNEL10, freq=300)
+    # actuonix_3.reset(pwm)
+    # actuonix_3.setPWM(pwm, dutycycle=60)
 
-    actuonix_4 = PCA9685(channel=CHANNEL11, freq=300)
-    actuonix_4.reset(pwm)
-    actuonix_4.setPWM(pwm, dutycycle=60)
+    # actuonix_4 = PCA9685(channel=CHANNEL11, freq=300)
+    # actuonix_4.reset(pwm)
+    # actuonix_4.setPWM(pwm, dutycycle=60)
     
     # SERVO
     turnigy_1 = PCA9685(channel=CHANNEL12, freq=300)
     turnigy_1.reset(pwm)
     turnigy_1.setPWM(pwm, dutycycle=28)
 
-    turnigy_2 = PCA9685(channel=CHANNEL13, freq=300)
-    turnigy_2.reset(pwm)
-    turnigy_2.setPWM(pwm, dutycycle=28)
+    # turnigy_2 = PCA9685(channel=CHANNEL13, freq=300)
+    # turnigy_2.reset(pwm)
+    # turnigy_2.setPWM(pwm, dutycycle=28)
     
     # DC BRUSHED
     pololu_0 = TB9051FTG(channel=CHANNEL0, freq=300, pin_in=MOTORS["pololu_0"]["enc_pins"], pin_out=MOTORS["pololu_0"]["driver_pins"], single=True)
@@ -62,22 +62,35 @@ def main():
     pololu_1 = TB9051FTG(channel=CHANNEL4, freq=300, pin_in=MOTORS["pololu_1"]["enc_pins"], pin_out=MOTORS["pololu_1"]["driver_pins"])
     pololu_1.reset(pwm)
 
-    pololu_2 = TB9051FTG(channel=CHANNEL5, freq=300, pin_in=MOTORS["pololu_2"]["enc_pins"], pin_out=MOTORS["pololu_2"]["driver_pins"])
-    pololu_2.reset(pwm)
+    # pololu_2 = TB9051FTG(channel=CHANNEL5, freq=300, pin_in=MOTORS["pololu_2"]["enc_pins"], pin_out=MOTORS["pololu_2"]["driver_pins"])
+    # pololu_2.reset(pwm)
 
-    pololu_3 = TB9051FTG(channel=CHANNEL6, freq=300, pin_in=MOTORS["pololu_3"]["enc_pins"], pin_out=MOTORS["pololu_3"]["driver_pins"])
-    pololu_3.reset(pwm)
+    # pololu_3 = TB9051FTG(channel=CHANNEL6, freq=300, pin_in=MOTORS["pololu_3"]["enc_pins"], pin_out=MOTORS["pololu_3"]["driver_pins"])
+    # pololu_3.reset(pwm)
 
-    pololu_4 = TB9051FTG(channel=CHANNEL7, freq=300, pin_in=MOTORS["pololu_4"]["enc_pins"], pin_out=MOTORS["pololu_4"]["driver_pins"])
-    pololu_4.reset(pwm)
+    # pololu_4 = TB9051FTG(channel=CHANNEL7, freq=300, pin_in=MOTORS["pololu_4"]["enc_pins"], pin_out=MOTORS["pololu_4"]["driver_pins"])
+    # pololu_4.reset(pwm)
 
     # INIT PID CONTROLLERS
     pid_1 = PID(MOTORS["pololu_1"]["enc_pins"], debug=True)
+    # pid_2 = PID(MOTORS["pololu_2"]["enc_pins"], debug=True)
+    # pid_3 = PID(MOTORS["pololu_3"]["enc_pins"], debug=True)
+    # pid_4 = PID(MOTORS["pololu_4"]["enc_pins"], debug=True)
 
     target = 0
     # target = 1
-    plate_closed = False
+
+    buttonA_pressed = False
+    buttonB_pressed = False
+    buttonX_pressed = False
     buttonY_pressed = False
+    joystick_pressed = False
+
+    plate_closed = False
+
+    # TEMP
+    wpi.pinMode(27, wpi.INPUT)
+    wpi.pullUpDnControl(27, wpi.GPIO.PUD_UP)
 
     try:
         while True:
@@ -88,11 +101,19 @@ def main():
             # pololu_1.forward(pwm, dutycycle=int(dc))
             # pololu_0.forward(pwm, dutycycle=int(dc))
 
+            # A B X Y
             buttonA = wpi.digitalRead(4)   # A
             buttonB = wpi.digitalRead(5)   # B
             buttonX = wpi.digitalRead(6)   # X
             buttonY = wpi.digitalRead(10)  # Y
             
+            # Joystick
+            js_sw = wpi.digitalRead(27)
+            js_vrx = wpi.analogRead(29)
+            js_vry = wpi.analogRead(25)
+
+            print(f"SW: {js_sw}, X: {js_vrx}, Y: {js_vry}")
+
             # ACTUATOR CONTROL
             # target position
             # if target < 30:
@@ -106,89 +127,50 @@ def main():
                     # actuonix_1.setPWM(pwm, dutycycle=target+30)
                     # time.sleep(0.15)
 
-            # SERVO CONTROL
-            while not buttonY:
-                buttonY_pressed = True
-                buttonY = wpi.digitalRead(10)  # Y
+            # # SERVO CONTROL
+            # while not buttonY:
+            #     buttonY_pressed = True
+            #     buttonY = wpi.digitalRead(10)  # Y
 
-            if buttonY_pressed:
-                if plate_closed:
-                    plate_closed = False
-                    turnigy_1.setPWM(pwm, dutycycle=28)
-                elif not plate_closed:
-                    plate_closed = True
-                    turnigy_1.setPWM(pwm, dutycycle=51)
+            # if buttonY_pressed:
+            #     if plate_closed:
+            #         plate_closed = False
+            #         turnigy_1.setPWM(pwm, dutycycle=28)
+            #     elif not plate_closed:
+            #         plate_closed = True
+            #         turnigy_1.setPWM(pwm, dutycycle=51)
 
-                buttonY_pressed = False
+            #     buttonY_pressed = False
 
-            # POLOLU ENCODED
-            if not buttonA:
-                target += 1
-            if not buttonB:
-                target -= 1
+            # # POLOLU ENCODED
+            # if not buttonA:
+            #     target += 1
+            # if not buttonB:
+            #     target -= 1
                 
-            # # TODO: create different classes pololu for turnigy & actuators too? to limit range!
-            pid_1.loop(target)
+            # # # TODO: create different classes pololu for turnigy & actuators too? to limit range!
+            # pid_1.loop(target)
 
-
-            # signal the motor
-            if pid_1.getDir() == -1:
-                pololu_1.forward(pwm, dutycycle=pid_1.getDc())
-            elif pid_1.getDir() == 1:
-                pololu_1.backward(pwm, dutycycle=pid_1.getDc())
+            # # signal the motor
+            # if pid_1.getDir() == -1:
+            #     pololu_1.forward(pwm, dutycycle=pid_1.getDc())
+            # elif pid_1.getDir() == 1:
+            #     pololu_1.backward(pwm, dutycycle=pid_1.getDc())
 
     except KeyboardInterrupt:
-        pololu_1.reset(pwm)
-        pololu_0.reset(pwm)
-        turnigy_1.reset(pwm)
         actuonix_1.reset(pwm)
+        # actuonix_2.reset(pwm)
+        # actuonix_3.reset(pwm)
+        # actuonix_4.reset(pwm)
+        turnigy_1.reset(pwm)
+        # turnigy_2.reset(pwm)
+        pololu_0.reset(pwm)
+        pololu_1.reset(pwm)
+        # pololu_2.reset(pwm)
+        # pololu_3.reset(pwm)
+        # pololu_4.reset(pwm)
         cleanup()
         sys.exit(0)
-
-    # while True:
-    #     motor = input("Enter p0 for pololu0, p1 for pololu1, a for actuator, t for turnigy: ")
-    #     if motor == "p0":
-    #         direction = input("Enter f for fwd, b for bkwd, s for stop: ")
-
-    #         if direction == "f":
-    #             print("Going forward")
-    #             pololu_0.forward(pwm, dutycycle=60)
-    #         elif direction == "b":
-    #             print("Going Backward")
-    #             pololu_0.backward(pwm, dutycycle=60)
-    #         elif direction == "s":
-    #             print("Stopping")
-    #             pololu_0.stop(pwm)
-    #     elif motor == "p1":
-    #         direction = input("Enter f for fwd, b for bkwd, s for stop: ")
-
-    #         if direction == "f":
-    #             print("Going forward")
-    #             pololu_1.forward(pwm, dutycycle=60)
-    #         elif direction == "b":
-    #             print("Going Backward")
-    #             pololu_1.backward(pwm, dutycycle=60)
-    #         elif direction == "s":
-    #             print("Stopping")
-    #             pololu_1.stop(pwm)
-    #     elif motor == "a":
-    #         direction = input("Enter o for out, i for in: ")
-
-    #         if direction == "i":
-    #             print("Going in")
-    #             actuonix_1.setPWM(pwm, dutycycle=30)
-    #         elif direction == "o":
-    #             print("Going out")
-    #             actuonix_1.setPWM(pwm, dutycycle=60)
-    #     elif motor == "t":
-    #         direction = input("Enter f for fwd, b for bkwd: ")
-
-    #         if direction == "f":
-    #             print("Going forward")
-    #             turnigy_1.setPWM(pwm, dutycycle=28)
-    #         elif direction == "b":
-    #             print("Going Backward")
-    #             turnigy_1.setPWM(pwm, dutycycle=64)
 
 def init_gpio():
     # unexport pins
