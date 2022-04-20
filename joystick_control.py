3f586a31f1ec6a77656df2ed3198b088ed40b808
Abeilles14 <40010108+Abeilles14@users.noreply.github.com>
2022-04-19 21:48:35 -0700

    encoder position test

--- joystick_control.py
+++ joystick_control.py
@@ -62,44 +62,101 @@ class JoystickControl:
         self.pololu_1 = TB9051FTG(channel=CHANNEL4, freq=300, pin_in=MOTORS["pololu_1"]["enc_pins"], pin_out=MOTORS["pololu_1"]["driver_pins"])
         self.pololu_1.reset(self.pwm)
 
-        self.pololu_2 = TB9051FTG(channel=CHANNEL5, freq=300, pin_in=MOTORS["pololu_2"]["enc_pins"], pin_out=MOTORS["pololu_2"]["driver_pins"])
-        self.pololu_2.reset(self.pwm)
+        # self.pololu_2 = TB9051FTG(channel=CHANNEL5, freq=300, pin_in=MOTORS["pololu_2"]["enc_pins"], pin_out=MOTORS["pololu_2"]["driver_pins"])
+        # self.pololu_2.reset(self.pwm)
 
-        self.pololu_3 = TB9051FTG(channel=CHANNEL6, freq=300, pin_in=MOTORS["pololu_3"]["enc_pins"], pin_out=MOTORS["pololu_3"]["driver_pins"])
-        self.pololu_3.reset(self.pwm)
+        # self.pololu_3 = TB9051FTG(channel=CHANNEL6, freq=300, pin_in=MOTORS["pololu_3"]["enc_pins"], pin_out=MOTORS["pololu_3"]["driver_pins"])
+        # self.pololu_3.reset(self.pwm)
 
-        self.pololu_4 = TB9051FTG(channel=CHANNEL7, freq=300, pin_in=MOTORS["pololu_4"]["enc_pins"], pin_out=MOTORS["pololu_4"]["driver_pins"])
-        self.pololu_4.reset(self.pwm)
+        # self.pololu_4 = TB9051FTG(channel=CHANNEL7, freq=300, pin_in=MOTORS["pololu_4"]["enc_pins"], pin_out=MOTORS["pololu_4"]["driver_pins"])
+        # self.pololu_4.reset(self.pwm)
 
         ########################
         # INIT PID CONTROLLERS #
         ########################
         uaslog.debug("Init PID controllers...")
-        self.pid_1 = PID(MOTORS["pololu_1"]["enc_pins"])
-        self.pid_2 = PID(MOTORS["pololu_2"]["enc_pins"])
-        self.pid_3 = PID(MOTORS["pololu_3"]["enc_pins"])
-        self.pid_4 = PID(MOTORS["pololu_4"]["enc_pins"])
+        self.pid_1 = PID(MOTORS["pololu_1"]["enc_pins"], debug=True)
+        # self.pid_2 = PID(MOTORS["pololu_2"]["enc_pins"])
+        # self.pid_3 = PID(MOTORS["pololu_3"]["enc_pins"])
+        # self.pid_4 = PID(MOTORS["pololu_4"]["enc_pins"])
 
         uaslog.info("Motor Drive System init complete! Starting main routine...")
         
     def loop(self):
+        uaslog.info("Starting Joystick Motor Control Test...")
+        uaslog.info("Joystick will control wheels to move forward or backward.")
+
         try:
             while True:
+
+                # button1 = wpi.digitalRead(PIN_A)
+                # button2 = wpi.digitalRead(PIN_B)
+                # # target position
+                self.target_pololu[1] = 1000
+                # if not button1:
+                #     self.target_pololu[1] += 1
+                # elif not button2:
+                #     self.target_pololu[1] -= 1
+
+                # enc_a = wpi.digitalRead(MOTORS["pololu_1"]["enc_pins"][0])
+                # enc_b = wpi.digitalRead(MOTORS["pololu_1"]["enc_pins"][1])
+
+                print(f"target: {self.target_pololu[1]}")
+                # print("ENC A: {}, ENC B: {}".format(enc_a, enc_b))
+                
                 # READ JOYSTICK
-                raw_ljs_x = wpi.analogRead(PIN_LJSX)
-                raw_ljs_y = wpi.analogRead(PIN_LJSY)
+                # raw_ljs_x = wpi.analogRead(PIN_LJSX)
+                # raw_ljs_y = wpi.analogRead(PIN_LJSY)
 
-                ljs_x, ljs_y = remap_range(raw_ljs_x, raw_ljs_y)
+                # ljs_x, ljs_y = remap_range(raw_ljs_x, raw_ljs_y)
+
+                # if ljs_y < 0.2 and ljs_y > -0.2:
+                #     ljs_y = 0.0
+                # if ljs_x < 0.2 and ljs_x > -0.2:
+                #     ljs_x = 0.0
                 
+                # print(f"sX: {ljs_x:.4f}, sY: {ljs_y:.4f}")
 
-                uaslog.info("Starting Joystick Motor Control Test...")
-                uaslog.info("Joystick will control wheels to move forward or backward.")
-
+                # # SET MOTOR TARGETS
+                # self.target_pololu[1] += ljs_y
+                # self.target_pololu[2] += ljs_y
+                # self.target_pololu[3] += ljs_y
+                # self.target_pololu[4] += ljs_y
                 
+                # print(f"target pos: [{self.target_pololu[1]:.4f}")
+                # print(f"target pos: [{self.target_pololu[1]:.4f}, {self.target_pololu[2]:.4f}, {self.target_pololu[3]:.4f}, {self.target_pololu[4]:.4f}]")
 
+                self.pid_1.loop(round(self.target_pololu[1]))
+                # self.pid_2.loop(round(self.target_pololu[2]))
+                # self.pid_3.loop(round(self.target_pololu[3]))
+                # self.pid_4.loop(round(self.target_pololu[4]))
+
+                # signal the motors
+                if self.pid_1.getDir() == -1:
+                    self.pololu_1.forward(self.pwm, dutycycle=self.pid_1.getDc())
+                elif self.pid_1.getDir() == 1:
+                    self.pololu_1.backward(self.pwm, dutycycle=self.pid_1.getDc())
+                
+                # if self.pid_2.getDir() == -1:
+                #     self.pololu_2.forward(self.pwm, dutycycle=self.pid_2.getDc())
+                # elif self.pid_2.getDir() == 1:
+                #     self.pololu_2.backward(self.pwm, dutycycle=self.pid_2.getDc())
+
+                # if self.pid_3.getDir() == -1:
+                #     self.pololu_3.forward(self.pwm, dutycycle=self.pid_3.getDc())
+                # elif self.pid_3.getDir() == 1:
+                #     self.pololu_3.backward(self.pwm, dutycycle=self.pid_3.getDc())
+
+                # if self.pid_4.getDir() == -1:
+                #     self.pololu_4.forward(self.pwm, dutycycle=self.pid_4.getDc())
+                # elif self.pid_4.getDir() == 1:
+                #     self.pololu_4.backward(self.pwm, dutycycle=self.pid_4.getDc())
+                
+                # print(f"curr pos: [{self.pid_1.getPos()}")
+                # print(f"curr pos: [{self.pid_1.getPos()}, {self.pid_2.getPos()}, {self.pid_3.getPos()}, {self.pid_4.getPos()}, ]")
                 
         except KeyboardInterrupt:
-            uaslog.info("Motor Isolation Test Complete!")
+            uaslog.info("Joystick Control Test Complete!")
             self.cleanup()
             sys.exit(0)
 
